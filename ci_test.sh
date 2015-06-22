c9b39c0e0bc4ba6f895909615f655495182614b4
jonathan <jonathan@jonathan>
2015-06-22 14:22:17 +0800

    * update ci script

--- ci_test.sh
+++ ci_test.sh
@@ -1,4 +1,8 @@
-mkdir -p ~/mb/webus
-meteor build --architecture=os.linux.x86_64  ~/mb/webus
-scp ~/mb/webus/webus.tar.gz root@m.kanbing365.com:/mb/webus
-ssh root@m.kanbing365.com
+git submodule init
+git submodule update
+ls -la
+meteor build --architecture=os.linux.x86_64  /tmp/webus
+docker rm -f mt_stagging
+docker run -d --name mt_stagging -e ROOT_URL="http://fitark.org:8061"  -e MONGO_URL="mongodb://root:root@101.200.228.46:27019,123.57.250.117:27017,123.57.250.222:27019/webus?replicaSet=thedb&authSource=admin"  -e MONGO_OPLOG_URL="mongodb://root:root@101.200.228.46:27019,123.57.250.117:27017,123.57.250.222:27019/local?replicaSet=thedb&authSource=admin"  -v /tmp/webus:/bundle  -p 8601:80 meteorhacks/meteord
+sleep 15
+curl http://fitark.org:8601/
