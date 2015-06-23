f492f4bddda4e5b8e396d4c5c8ee8583e6e397b0
Weiming Zhai <wmzhai@gmail.com>
2015-06-23 11:18:46 +0800

    build 623

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 git pull origin develop
-mkdir -p ~/mb/webus
-meteor build --architecture=os.linux.x86_64  ~/mb/webus
-scp ~/mb/webus/webus.tar.gz root@m.kanbing365.com:/mb/webus
+mkdir -p ~/mb/webus623
+meteor build --architecture=os.linux.x86_64  ~/mb/webus623
+scp ~/mb/webus623/webus.tar.gz root@m.kanbing365.com:/mb/webus623
 ssh root@m.kanbing365.com
